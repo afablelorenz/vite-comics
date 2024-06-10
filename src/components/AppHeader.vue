ae3bf7bf2ab7d1f199120c00af2e74a9806267d0
afablelorenz <afablelorenz@gmail.com>
2024-06-10 18:02:04 +0200

    update appheader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<script>
+export default{
+    data(){
+        return{
+            navLinks:[
+                {text: 'Characters'},
+                {text: 'Comisc'},
+                {text: 'Movies'},
+                {text: 'TV'},
+                {text: 'Games'},
+                {text: 'Collectibles'},
+                {text: 'Videos'},
+                {text: 'Fans'},
+                {text: 'News'},
+                {text: 'Shop'},
+            ]
+        }
+    }
+}
+
+</script>
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li v-for="link in navLinks" :keys="link.text">
+                    <a href="">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+<style scoped>
+
+header{
+    background-color: white;
+    padding:10px 0
+}
+</style>
